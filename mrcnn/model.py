2d16f7799c8db37c800694b661ee9ab0a988bd40
Georgios <georgios.ouzounis@gmail.com>
2021-07-12 01:02:16 +0300

    line 2268 added log/ in self.log_dir

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2265,7 +2265,8 @@ class MaskRCNN():
 
 
         #  Directory for training logs
-        self.log_dir = os.path.join(self.model_dir, "{}{:%Y%m%dT%H%M}".format(
+        self.log_dir = os.path.join(self.model_dir, "log/")
+        self.log_dir = os.path.join(self.log_dir, "{}{:%Y%m%dT%H%M}".format(
             self.config.NAME.lower(), now))
         # Path to save after each epoch. Include placeholders that get filled by Keras.
         self.checkpoint_path = os.path.join(self.log_dir, "mask_rcnn_{}_*epoch*.h5".format(
